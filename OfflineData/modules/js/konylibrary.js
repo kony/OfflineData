733f92e61e1f93e2eb7c8214c56696d08c54a95e
KonyDev <SampleAppsTeam@kony.com>
2016-06-13 12:47:21 +0530

    upgarded for 6.5 & working for 7.0GA

--- OfflineData/modules/js/konylibrary.js
+++ OfflineData/modules/js/konylibrary.js
@@ -573,8 +573,8 @@ kony.string = {
 		if (arguments.length === 0) {
 			throw new Error("string.isAsciiAlphaNumeric needs atleast 1 argument");
 		}
-		var p1 = /[^a-z0-9]/i;	// Elimimate non alpha numeric chars
-		var p2 = /([a-z]+[0-9])|([0-9]+[a-z])/i;
+		var p1 = /[^a-zA-Z0-9]/i;	// Elimimate non alpha numeric chars //Added cap letters also 
+		var p2 = /^[a-zA-Z0-9]*$/i; /* Wrong regex   /([a-z]+[0-9])|([0-9]+[a-z])/i; */
 		var r1 = str.match(p1);
 		var r2 = str.match(p2);	
 		return (!r1 && r2 && str) ? true : false;	
@@ -705,6 +705,9 @@ kony.string = {
 			return false;
 		//(99.9% syntactic accuracy)
 		var emailPattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+		if((value.length - value.lastIndexOf('.')) < 3){
+			return false;
+		}
 
 		return emailPattern.test(value);
 	},
